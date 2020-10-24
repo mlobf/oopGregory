8cbafc74deecf5f89a0040ee7091405f4f8e3259
marcos <marcoslemeborbafilho@gmail.com>
2020-10-24 17:09:51 -0300

    commit

--- metodos.py
+++ metodos.py
@@ -4,6 +4,8 @@
 classe 
 objetos
 construtor
+    E o init, este quando ja implementado, ja cria uma um objeto
+    com as suas respectivas propriedades.
 metodos
     Quando uma classe tem uma funçao, esta e chamada de metodo.
 atributos
@@ -23,19 +25,35 @@ Isso e uma classe.
 # Vou criar realmente na memoria no pc uma "pessoal" oriunda da classe.
 #Atributos de uma classe sao os 'adjetivos desta. No caso de uma Classe pessoa
 #   temos como possiveis atributos, nome, idade, sexo.
-
 class Casa(object):
     cor = "Amarela"
     altura = 3
     quartos = 10
 
+    def __init__(self, cor, altura, quartos):
+        self.cor = cor
+        self.altura = altura
+        self.quartos = quartos
+
     def pintar(self, cor):
         self.cor = cor
 
-#Vou criar um objeto.
-minha_casa = Casa()     #Agora a classe foi instanciada. Ela passou a existir.
-print(minha_casa.cor, str(minha_casa.altura))
+    def aumentar_quartos(self, quartos):
+        self.quartos = quartos
 
-print(minha_casa.pintar('branco'))
+    def imprime_casa(self):
+        print(self.cor,self.altura,self.quartos)
 
-print('oi')
+
+minha_casa = Casa('vermelha',2,23)
+
+minha_casa.imprime_casa()
+
+
+
+
+
+
+
+
+
